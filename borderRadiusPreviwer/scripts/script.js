991c1ec2aeb81dd06740cc4de0ed7c25ba7fe524
Alex Junior <alexjr2oficial@gmail.com>
2022-10-18 14:41:53 -0300

    finishing the script

--- borderRadiusPreviwer/scripts/script.js
+++ borderRadiusPreviwer/scripts/script.js
@@ -13,44 +13,44 @@ let borderBLEFT2 = document.getElementById('borderBLEFT2');
 let borderBRIGHT = document.getElementById('borderBRIGHT'); //botton right
 let borderBRIGHT2 = document.getElementById('borderBRIGHT2');
 
+this.onload = borderPreviwer.borderRadius = '50% 17% 30% 73%'
 
+// top left
+borderTLEFT.addEventListener("change", (event) => {
+  borderPreviwer.borderTopLeftRadius = `${event.target.value}% \ ${borderTLEFT2.value}%`
+  console.log("TL", event.target.value, borderTLEFT2.value)
+});
+borderTLEFT2.addEventListener("change", (event) => {
+  borderPreviwer.borderTopLeftRadius = `${borderTLEFT.value}% \ ${event.target.value}% `
+  console.log("TL", borderTLEFT.value, event.target.value)
+});
 
+// top right
+borderTRIGHT.addEventListener("change", (event) => {
+  borderPreviwer.borderTopRightRadius = `${event.target.value}% \ ${borderTRIGHT2.value}%`
+  console.log("TR", event.target.value, borderTRIGHT2.value)
+});
+borderTRIGHT2.addEventListener("change", (event) => {
+  borderPreviwer.borderTopRightRadius = `${borderTRIGHT.value}% \ ${event.target.value}% `
+  console.log("TR", borderTRIGHT.value, event.target.value)
+});
 
+// botton left
+borderBLEFT.addEventListener("change", (event) => {
+  borderPreviwer.borderBottomLeftRadius = `${event.target.value}% \ ${borderBLEFT2.value}%`
+  console.log("TR", event.target.value, borderBLEFT2.value)
+});
+borderBLEFT2.addEventListener("change", (event) => {
+  borderPreviwer.borderBottomLeftRadius = `${borderBLEFT.value}% \ ${event.target.value}% `
+  console.log("TR", borderBLEFT.value, event.target.value)
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function topL() {
-//   borderPreviwer.borderTopLeftRadius = `${borderTLEFT.ariaValueNow}% \ ${borderTLEFT2.ariaValueNow}%`;
-//   console.log('topL: ', borderTLEFT.value, borderTLEFT2.value)
-// }
-// function topR() {
-//   borderPreviwer.borderTopRightRadius = `${borderTRIGHT}% \ ${borderTRIGHT2}%`;
-//   console.log('topR: ', borderTRIGHT, borderTRIGHT2)
-// }
-// function bottonL() {
-//   borderPreviwer.borderBottomLeftRadius = `${borderBLEFT}% \ ${borderBLEFT2}%`;
-//   console.log('bottonL: ', borderBLEFT, borderBLEFT2)
-// }
-// function bottonR() {
-//   borderPreviwer.borderBottomRightRadius = `${borderBRIGHT}% \ ${borderBRIGHT2}%`;
-//   console.log('bottonR: ', borderBRIGHT, borderBRIGHT2)
-// }
+//botton right
+borderBRIGHT.addEventListener("change", (event) => {
+  borderPreviwer.borderBottomRightRadius = `${event.target.value}% \ ${borderBRIGHT2.value}%`
+  console.log("TR", event.target.value, borderBRIGHT2.value)
+});
+borderBRIGHT2.addEventListener("change", (event) => {
+  borderPreviwer.borderBottomRightRadius = `${borderBRIGHT.value}% \ ${event.target.value}% `
+  console.log("TR", borderBLEFT.value, event.target.value)
+});
