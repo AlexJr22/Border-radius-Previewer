5683cbd51c797664e8164d0b8c6c427c4b17e163
Alex Junior <alexjr2oficial@gmail.com>
2022-10-18 13:34:35 -0300

    first attempt to script

---
+++ borderRadiusPreviwer/scripts/script.js
@@ -0,0 +1,56 @@
+const borderPreviwer = document.getElementById('borderPreviwer').style;
+
+// inputs 
+let borderTLEFT = document.getElementById('borderTLEFT'); // top left
+let borderTLEFT2 = document.getElementById('borderTLEFT2');
+
+let borderTRIGHT = document.getElementById('borderTRIGHT'); // top right
+let borderTRIGHT2 = document.getElementById('borderTRIGHT2');
+
+let borderBLEFT = document.getElementById('borderBLEFT');// botton left
+let borderBLEFT2 = document.getElementById('borderBLEFT2');
+
+let borderBRIGHT = document.getElementById('borderBRIGHT'); //botton right
+let borderBRIGHT2 = document.getElementById('borderBRIGHT2');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function topL() {
+//   borderPreviwer.borderTopLeftRadius = `${borderTLEFT.ariaValueNow}% \ ${borderTLEFT2.ariaValueNow}%`;
+//   console.log('topL: ', borderTLEFT.value, borderTLEFT2.value)
+// }
+// function topR() {
+//   borderPreviwer.borderTopRightRadius = `${borderTRIGHT}% \ ${borderTRIGHT2}%`;
+//   console.log('topR: ', borderTRIGHT, borderTRIGHT2)
+// }
+// function bottonL() {
+//   borderPreviwer.borderBottomLeftRadius = `${borderBLEFT}% \ ${borderBLEFT2}%`;
+//   console.log('bottonL: ', borderBLEFT, borderBLEFT2)
+// }
+// function bottonR() {
+//   borderPreviwer.borderBottomRightRadius = `${borderBRIGHT}% \ ${borderBRIGHT2}%`;
+//   console.log('bottonR: ', borderBRIGHT, borderBRIGHT2)
+// }
